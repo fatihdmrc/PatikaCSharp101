2f480387f9ebf7891645c405f29b546a729e87a3
Fatih Demirci <fathdmrc01@gmail.com>
2024-12-16 14:09:11 +0300

    Ödev 3 eklendi

---
+++ Ödev3/Program.cs
@@ -0,0 +1,8 @@
+﻿ static int solveMeFirst(int a, int b)
+{ 
+        return a+b;
+}
+int val1 = Convert.ToInt32(Console.ReadLine());
+int val2 = Convert.ToInt32(Console.ReadLine());
+int sum = solveMeFirst(val1,val2);
+Console.WriteLine(sum);
