8ef89a28b328e17edd2d0fd03b68894b0c55b9f3
Fatih Demirci <fathdmrc01@gmail.com>
2024-12-17 14:00:37 +0300

    Ödev5 eklendi

---
+++ Ödev5/Program.cs
@@ -0,0 +1,16 @@
+﻿ int N = Convert.ToInt32(Console.ReadLine().Trim());
+        if(N%2==1)
+        {
+            Console.WriteLine("Weird");
+        }
+        else if(N>=2&&N<=5){
+            Console.WriteLine("Not Weird");
+        }
+        else if(N>=6&&N<=20)
+        {
+            Console.WriteLine("Weird");
+        }
+        else if(N>20)
+        {
+            Console.WriteLine("Not Weird");
+        }
