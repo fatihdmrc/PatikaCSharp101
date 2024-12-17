34e3581d7c41ee12dcf0745826d3d487b2b2c3c1
Fatih Demirci <fathdmrc01@gmail.com>
2024-12-17 11:45:17 +0300

    Ödev 4 eklendi

---
+++ Ödev4/Program.cs
@@ -0,0 +1,14 @@
+﻿
+try
+{
+System.Console.WriteLine("Lütfen yaşınızı girin:");
+byte yas = Convert.ToByte(Console.ReadLine());
+}
+catch (FormatException ex)
+{
+    System.Console.WriteLine("Hata:Geçersiz değer girildi!");
+}
+catch (OverflowException ex2)
+{
+    System.Console.WriteLine("Girilebilecek değer aralığı dışına çıkıldı!");
+}
