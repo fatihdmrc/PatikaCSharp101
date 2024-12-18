7a91bc22fca2a3e850d7495aaf976fb93e75082b
Fatih Demirci <fathdmrc01@gmail.com>
2024-12-18 13:38:00 +0300

    Ödev8 eklendi

---
+++ Ödev8/Program.cs
@@ -0,0 +1,13 @@
+﻿// While Döngüsü
+char harf = 'a';
+while (harf<='z')
+{
+    System.Console.WriteLine(harf);
+    harf++;
+}
+// Foreach döngüsü
+string[] arabaMarkalari = {"Mercedes","BMW","Audi","Toyota","Hyundai","Fiat","Renault"};
+foreach (string item in arabaMarkalari)
+{
+    System.Console.WriteLine(item);
+}
