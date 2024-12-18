94a2c9a804172132f97941ad3994121caffcf458
Fatih Demirci <fathdmrc01@gmail.com>
2024-12-18 20:27:50 +0300

    Migratory Birds çözümü düzenlendi

--- Ödev 9/Migratorybirds.cs
+++ Ödev 9/Migratorybirds.cs
@@ -1,79 +1,40 @@
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
-
-class Sonuc2
-{
-
-    /*
-     * Complete the 'migratoryBirds' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts INTEGER_ARRAY arr as parameter.
-     */
-
-    public static int migratoryBirds(List<int> arr)
-    {
-        var birdIdCounts = new Dictionary<int, int>();
-        
-        foreach(var birdId in arr)
+static int migratoryBirds(List<int> arr)
+    {   
+       Dictionary<int,int> frekansSozluk = new Dictionary<int, int>();
+       foreach(var item in arr) // Hangi sayıdan kaç tane var sözlüğe ekledik
+       {
+        if(frekansSozluk.ContainsKey(item))
         {
-            if(birdIdCounts.ContainsKey(birdId))
+            frekansSozluk[item]++;
+        }
+        else
+        {
+            frekansSozluk.Add(item,1);
+        }
+       } 
+       int maxdeger = 0;
+       foreach (var item in frekansSozluk)
+       {
+            if(item.Value>maxdeger)
             {
-                birdIdCounts[birdId]++;
+                maxdeger = item.Value;
             }
-            else
-            {
-                birdIdCounts[birdId] = 1;
-            }
+       }
+       List<int> maxFrekanslar = new List<int>();
+       foreach (var item in frekansSozluk)
+       {
+        if(item.Value == maxdeger)
+        {
+            maxFrekanslar.Add(item.Key);
         }
-        
-        var maxValue = 0;
-        
-        foreach (var birdIdCount in birdIdCounts) {
-            if(birdIdCount.Value > maxValue) {
-                maxValue = birdIdCount.Value;
-            }
-        }
-        
-        var birdIdsWithMaxValue = new List<int>();
-        
-        foreach(var birdIdCount in birdIdCounts) {
-            if(birdIdCount.Value == maxValue) {
-                birdIdsWithMaxValue.Add(birdIdCount.Key);
-            }
-        }
-        
-        return birdIdsWithMaxValue.Min();
+       }
+       return maxFrekanslar.Min();
+
     }
-
-}
-
-class Cozum4
-{
-    public static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
         int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
 
         List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
 
-        int result = Sonuc2.migratoryBirds(arr);
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
-    }
-}
+        int result = migratoryBirds(arr);
+        System.Console.WriteLine(result);
