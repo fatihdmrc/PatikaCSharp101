edd7bfba1967f162b059c60ec4c88fb5aa6113dd
Fatih Demirci <fathdmrc01@gmail.com>
2024-12-20 16:07:07 +0300

    Ödev10 düzenlendi

--- Ödev10/Program.cs
+++ Ödev10/Program.cs
@@ -22,3 +22,13 @@
         Console.WriteLine(buyukoran.ToString("F6"));
         Console.WriteLine(kucukoran.ToString("F6"));
         Console.WriteLine(sifiroran.ToString("F6"));
+        System.Console.WriteLine("Kaç adet sayı gireceksin?:");
+    int adet = Convert.ToInt32(Console.ReadLine());
+    List<int> sayilar = new List<int>();
+    for(int i=0; i<adet; i++){
+        System.Console.WriteLine("Lütfen "+(i+1)+". sayıyı girin:");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+        sayilar.Add(sayi);
+    }
+    oranHesaplayici(sayilar);
+   
