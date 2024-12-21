6826f9dea53efb2994f5035409b2437deaa673c8
Fatih Demirci <fathdmrc01@gmail.com>
2024-12-22 01:38:37 +0300

    Ödev 13 eklendi

---
+++ Ödev13/Program.cs
@@ -0,0 +1,18 @@
+﻿System.Console.WriteLine("Faktöriyeli alınacak sayıyı giriniz:");
+int sayi = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(factorial(sayi));
+static int factorial(int n)
+{
+    if(n==0)
+            {
+                return 1;
+            }
+            else if(n==1)
+            {
+                return 1;
+            }
+            else {
+            
+                return  n*factorial(n-1) ;
+            }
+}
