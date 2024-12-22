69609a55e76d5dbb2003d09808d9332b4414b7f6
Fatih Demirci <fathdmrc01@gmail.com>
2024-12-22 18:00:49 +0300

    Ödev15 eklendi

---
+++ Odev15/HackerrankDay6.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+public static class Solution {
+    public static string ciftIndeksleriGetir(this string deger) 
+    {   
+        string yeni="";
+        for (int i=0; i<deger.Length; i=i+2)
+        {
+           yeni = yeni + deger[i];
+        }
+        return yeni;
+    }
+    public static string tekIndeksleriGetir(this string deger) 
+    {
+        string yeni = "";
+         for (int i=1; i<deger.Length; i=i+2)
+        {
+           yeni = yeni + deger[i];
+        }
+        return yeni;
+    }
+    static void Main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] kelimeler = new string[n];
+        for(int i=0; i<n; i++) 
+        {
+            kelimeler[i]= Console.ReadLine();
+        }
+        for(int i=0; i<n; i++)
+        {
+            Console.WriteLine(kelimeler[i].ciftIndeksleriGetir()+" "+kelimeler[i].tekIndeksleriGetir());
+        }
+        
+    }
+}
