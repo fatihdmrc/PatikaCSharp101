648a416273c45e9cc0792b36eea5d2d8e7d36f0a
Fatih Demirci <fathdmrc01@gmail.com>
2025-02-21 22:44:38 +0300

    Ödev 18 eklendi

---
+++ Ödev18/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+ArrayList liste = new ArrayList();
+int[] sayilar = { 1, 2, 88, 15, 24, 96, 45 };
+liste.AddRange(sayilar);
+liste.Sort();
+foreach (int sayi in liste) // Sıralanmış ArrayList'i yazdırıyoruz
+{
+    System.Console.WriteLine(sayi);
+}
+System.Console.WriteLine(liste.BinarySearch(88)); // 88 elemanının indeksini ekranda yazdırır
+liste.Reverse();// ArrayList elemanlarının sırasını terse çevirir
+foreach (int sayi in liste)
+{
+    System.Console.WriteLine(sayi);
+}
+liste.Clear(); // Listeyi temizler
